e0e1fac27362e523177be264f539c9993ad9e890
Joel M <hj923@hotmail.com>
2018-10-29 14:49:50 +0000

    input successful

---
+++ constants.js
@@ -0,0 +1,12 @@
+const { Map } = require("immutable");
+
+const C = Map({
+  HKU_LOGIN_LINK:
+    "https://hkuportal.hku.hk/cas/servlet/edu.yale.its.tp.cas.servlet.Login?service=http://booking.its.hku.hk/lebook/book/Web/",
+  USERNAME: "#username",
+  PASSWORD: "#password",
+  LOGIN_BUTTON:
+    "#round-corner-1 > div > form > table > tbody > tr:nth-child(1) > td:nth-child(2) > table > tbody > tr > td > input"
+});
+
+module.exports = C;
--- index.js
+++ index.js
@@ -1,31 +1,76 @@
 const puppeteer = require("puppeteer");
 const readline = require("readline");
-
-const C = {
-  HKU_LOGIN: {
-    LINK:
-      "https://hkuportal.hku.hk/cas/servlet/edu.yale.its.tp.cas.servlet.Login?service=http://booking.its.hku.hk/lebook/book/Web/",
-    USERNAME: "#username",
-    PASSWORD: "#password"
-  }
-};
-
-const start = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(C.HKU_LOGIN.LINK);
-  let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-
-  console.log(bodyHTML);
-  await browser.close();
-};
+const read = require("read");
+const constants = require("./constants");
+const chalk = require("chalk");
+const { log } = console;
 
 // array: [selector0, inputValue0, selector1, inputValue1, ...]
-const insertLoginInfo = async array => {
+async function insertLoginInfo(page, array) {
   for (let i = 0; i < array.length / 2; i++) {
-    await page.focus("#lst-ib");
-    page.type("China");
+    await page.focus(array[i * 2]);
+    page.type(array[i * 2 + 1]);
   }
+}
+
+function readInput(option) {
+  return new Promise((resolve, reject) =>
+    read(option, (err, assignVarFrom) => {
+      if (err) {
+        reject(err);
+      }
+      console.log(assignVarFrom);
+      resolve(assignVarFrom);
+    })
+  );
+}
+
+const start = async () => {
+  /*
+  
+  Init
+  
+  */
+  const browser = await puppeteer.launch({ args: ["--disable-dev-shm-usage"] });
+  const page = await browser.newPage();
+  /*
+  
+  HKU LOGIN PART
+  
+  */
+  log(constants.get("HKU_LOGIN_LINK"));
+
+  log(constants.get("USERNAME"));
+
+  log(constants.get("PASSWORD"));
+
+  log(constants.get("LOGIN_BUTTON"));
+  await page.goto(constants.get("HKU_LOGIN_LINK"));
+  await page.evaluate(
+    () => {
+      document.querySelector("#username").value = "yo1o";
+      document.querySelector("#password").value = "test";
+    },
+    "yo1o",
+    "test"
+  );
+
+  const username = await page.evaluate(
+    () => document.querySelector("#username").value
+  );
+  const password = await page.evaluate(
+    () => document.querySelector("#password").value
+  );
+
+  log(
+    chalk.green(`:: Value Check ::
+  username: ${username}
+  password: ${password}
+  `)
+  );
+  log(page.url());
+
+  await browser.close();
 };
 
 start();
