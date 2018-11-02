fc8098ff8539a59ce099bf4f66355a2b21237132
Joel M <hj923@hotmail.com>
2018-11-02 12:00:50 +0000

    refactor index.js

--- index.js
+++ index.js
@@ -1,112 +1,95 @@
 const puppeteer = require("puppeteer");
-const readline = require("readline");
-const read = require("read");
+const inquirer = require("inquirer");
+const ora = require("ora");
 const C = require("./constants");
-const chalk = require("chalk");
-const { log } = console;
+const lib = require("./lib");
+const creds = require("./credentials");
+const log = require("./helper");
 
-// array: [selector0, inputValue0, selector1, inputValue1, ...]
-async function insertInfo(page, array) {
-  for (let i = 0; i < array.length / 2; i++) {
-    await page.evaluate((array, i) => {
-      console.log(
-        `${document.querySelector(array[i * 2]).value} = ${array[i * 2 + 1]}`
-      );
-      document.querySelector(array[i * 2]).value = array[i * 2 + 1];
-    }, array[i * 2 + 1]);
-  }
-}
-
-function readInput(option) {
-  return new Promise((resolve, reject) =>
-    read(option, (err, assignVarFrom) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(assignVarFrom);
-    })
-  );
-}
-
-function checkURL(page) {
-  log(chalk.bgBlue("Current URL: " + page.url()));
-}
-
-const start = async () => {
-  /*
-  
-  Init
-  
-  */
-  log(chalk.bgGreen("Launching browser and page..."));
-  const browser = await puppeteer.launch({ args: ["--disable-dev-shm-usage"] });
-  const page = await browser.newPage();
-  /*
-  
-  HKU LOGIN PART
-  
-  */
-  /*
-  1. Go onto HKU Portal Login Link that would redirect to booking system
-  */
-  await page.goto(C.get("HKU_LOGIN_LINK"));
-  checkURL(page);
-  /*
-  2. Receive user inputs for id and pw
-  */
-  let username = await readInput({
-    prompt: "Please enter your HKU Portal ID: "
-  });
-  let password = await readInput({
-    prompt: "Please enter your HKU Portal PIN: ",
-    silent: true
-  });
-  /*
-  3. Inject those info into the webpage
-  */
-  await page.evaluate(
-    ({ username, password }) => {
-      document.querySelector("#username").value = username;
-      document.querySelector("#password").value = password;
+const askQs = () => {
+  const questions = [
+    {
+      name: "DATE",
+      type: "list",
+      message: "Tell me which date you want to use the room",
+      choices: lib.getDates()
     },
     {
-      username,
-      password
+      name: "TIME",
+      type: "list",
+      message: "Tell me which time you want to use the room",
+      choices: lib.getDates()
+    },
+    {
+      name: "ROOM",
+      type: "list",
+      message: "Tell me which room you want to use",
+      pageSize: 10,
+      choices: lib.getRooms()
     }
-  );
-  /*
-  4. Check info
-  */
-  const un = await page.evaluate(
-    () => document.querySelector("#username").value
-  );
-  const pw = await page.evaluate(
-    () => document.querySelector("#password").value
-  );
+  ];
+  return inquirer.prompt(questions);
+};
+function checkURL(page) {
+  log.info("Current URL: " + page.url());
+}
 
-  if (!(un && pw)) {
-    // something's wrong
-    log(chalk.bgRed("You entered an invalid input. Please try again."));
-  } else {
-    log(chalk.bgGreen("Username and password check done. Good to go."));
-  }
+const test = async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage();
+  log.success("cool")
+}
+
+const run = async () => {
+  const spinner = ora().start();
+  spinner.info("Launching browser and page...");
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox', "--disable-dev-shm-usage"]});
+  const page = await browser.newPage();
   /*
-  5. Click login button
-  */
-  await Promise.all([
-    page.click(C.get("LOGIN_BUTTON")),
-    page.waitForNavigation({ waitUntil: "domcontentloaded" })
-  ]);
-  /* 
-  6. wait until redirection
-  */
-  await page
-    .waitForSelector(".schedule_title", { timeout: 1000 * 10 })
-    // one of the selectors in the page (http://booking.its.hku.hk/lebook/book/Web/schedule.php)
-    .then(() => log(chalk.bgGreen("Page redirection successful")));
-  // await page.waitFor(6000);
+    1. Go onto HKU Portal Login Link that would redirect to booking system
+    */
+  spinner.info("Going onto HKU Login page...");
+  await page.goto(C.HKU_LOGIN_LINK);
   checkURL(page);
+  /*
+    2. If the user already typed in credentials information, go right onto the booking page and fetch booking information
+    */
+  if (creds.username && creds.password) {
+    spinner.info(
+      "Username and password preconfigured in credentials.js found. Attempting to log in..."
+    );
+
+    await page.evaluate(
+      ({ username, password }) => {
+        document.querySelector("#username").value = username;
+        document.querySelector("#password").value = password;
+      },
+      {
+        username: creds.username,
+        password: creds.password
+      }
+    );
+
+    await Promise.all([
+      page.click(C.get("LOGIN_BUTTON")),
+      page.waitForNavigation({ waitUntil: "domcontentloaded" })
+    ]);
+
+    await page
+      .waitForSelector(".schedule_title", { timeout: 1000 * 10 })
+      // one of the selectors in the page (http://booking.its.hku.hk/lebook/book/Web/schedule.php)
+      .then(() => spinner.succeed("Login successful!"));
+    // await page.waitFor(6000);
+    checkURL(page);
+  } else {
+  }
   await browser.close();
+  const answers = await askQs();
+  const { CHOICE, ROOM, DATE, TIME } = answers;
 };
 
-start();
+try {
+  run();
+} catch (e) {
+  console.log(e);
+}
