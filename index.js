590ec7ebec3c74efb420e6f225013b11649afceb
9oelM <hj923@hotmail.com>
2018-11-17 15:00:13 +0900

    work up to login success

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const { success, warning, info, safeRequire } = require("./helper");
 const puppeteer = require("puppeteer");
 const inquirer = require("inquirer");
 const ora = require("ora");
 const C = require("./constants");
 const lib = require("./lib");
-const creds = require("./credentials");
-const log = require("./helper");
+let creds = safeRequire(
+  "./credentials.js",
+  "Credentials.js was not found. You have to manually type in your username and password.",
+  {}
+);
 
 const askQs = () => {
   const questions = [
@@ -30,20 +34,40 @@ const askQs = () => {
   ];
   return inquirer.prompt(questions);
 };
-function checkURL(page) {
-  log.info("Current URL: " + page.url());
-}
 
-const test = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage();
-  log.success("cool")
-}
+const checkURL = page => info("Current URL: " + page.url());
 
 const run = async () => {
+  if (!creds.username || !creds.password) {
+    const { USERNAME, PASSWORD } = await inquirer.prompt([
+      {
+        name: "USERNAME",
+        type: "input",
+        message: "Tell me your HKU portal username: "
+      },
+      {
+        name: "PASSWORD",
+        type: "password",
+        message: "Tell me your HKU portal password: "
+      }
+    ]);
+    creds = {
+      ...creds,
+      username: USERNAME,
+      password: PASSWORD
+    };
+  }
+
   const spinner = ora().start();
-  spinner.info("Launching browser and page...");
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox', "--disable-dev-shm-usage"]});
+  spinner.info("Launching browser and page. If you have chrome, chrome will automatically launch and do things for you.");
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+    ]
+  });
   const page = await browser.newPage();
   /*
     1. Go onto HKU Portal Login Link that would redirect to booking system
@@ -54,38 +78,33 @@ const run = async () => {
   /*
     2. If the user already typed in credentials information, go right onto the booking page and fetch booking information
     */
-  if (creds.username && creds.password) {
-    spinner.info(
-      "Username and password preconfigured in credentials.js found. Attempting to log in..."
-    );
+  spinner.info("Attempting to log in...");
 
-    await page.evaluate(
-      ({ username, password }) => {
-        document.querySelector("#username").value = username;
-        document.querySelector("#password").value = password;
-      },
-      {
-        username: creds.username,
-        password: creds.password
-      }
-    );
+  await page.evaluate(
+    ({ username, password }) => {
+      document.querySelector("#username").value = username;
+      document.querySelector("#password").value = password;
+    },
+    {
+      username: creds.username, 
+      password: creds.password 
+    }
+  );
 
-    await Promise.all([
-      page.click(C.get("LOGIN_BUTTON")),
-      page.waitForNavigation({ waitUntil: "domcontentloaded" })
-    ]);
+  await Promise.all([
+    page.click(C.LOGIN_BUTTON),
+    page.waitForNavigation({ waitUntil: "domcontentloaded" })
+  ]);
 
-    await page
-      .waitForSelector(".schedule_title", { timeout: 1000 * 10 })
-      // one of the selectors in the page (http://booking.its.hku.hk/lebook/book/Web/schedule.php)
-      .then(() => spinner.succeed("Login successful!"));
-    // await page.waitFor(6000);
-    checkURL(page);
-  } else {
-  }
-  await browser.close();
+  await page
+    .waitForSelector(".schedule_title", { timeout: 1000 * 10 })
+    // one of the selectors in the page (http://booking.its.hku.hk/lebook/book/Web/schedule.php)
+    .then(() => spinner.succeed("Login successful!"));
+  // await page.waitFor(6000);
+  checkURL(page);
   const answers = await askQs();
   const { CHOICE, ROOM, DATE, TIME } = answers;
+  await browser.close();
 };
 
 try {
