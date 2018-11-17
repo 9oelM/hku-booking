8d30ebded46b73082b0177111bedac6f42d543bd
9oelM <hj923@hotmail.com>
2018-11-17 15:00:00 +0900

    add saferequire

--- helper.js
+++ helper.js
@@ -6,3 +6,13 @@ exports.success = content => log(chalk.bgGreen(content));
 exports.warning = content => log(chalk.bgRed(content));
 
 exports.info = content => log(chalk.bgBlue(content));
+
+exports.safeRequire = (modulePath, errorMsg, fallbackObj) => { // force require
+    try {
+     return require(modulePath);
+    }
+    catch (e) {
+     log(chalk.bgBlackBright(errorMsg))
+     return fallbackObj;
+    }
+}
